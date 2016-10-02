747d356f2163ff8519bc9ea71542179b6b500d28
Donovan Allen <dallen.928@gmail.com>
2016-10-02 16:20:23 -0400

    added matching game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,18 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+if (cardFour === cardThree) {
+alert ("You found a match!");
+}
+
+else if (cardOne === cardTwo) {
+alert("You found a match!");
+}
+
+else {
+alert ("Sorry, try again");
+}
